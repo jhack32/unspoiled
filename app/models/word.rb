563f3c1bcfcbb284c98cb8f9a608459b7148a5f8
Jack Huang <Jack7100@gmail.com>
2016-01-25 18:33:13 -0500

    Add logic taken out from word controller to create keywords

--- app/models/word.rb
+++ app/models/word.rb
@@ -1,4 +1,11 @@
 class Word < ActiveRecord::Base
   belongs_to :preference
 
+  def self.create_words(words_array, custom_filter_obj)
+    words_array.each do |word|
+      if !word.match(/\d/) && !word.blank?
+        Word.create!(word: word.downcase.strip, preference: custom_filter_obj)
+      end
+    end
+  end
 end
